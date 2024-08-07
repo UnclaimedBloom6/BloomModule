7eb53114db7c5a279b9338a49c6997f994f1bcf9
UnclaimedBloom6 <unclaimedgamer@gmail.com>
2024-08-08 05:57:42 +1200

    Changed crystal timer to use chat packet

--- features/CrystalTimer.js
+++ features/CrystalTimer.js
@@ -1,10 +1,12 @@
 import Dungeon from "../../BloomCore/dungeons/Dungeon";
+import { onChatPacket } from "../../BloomCore/utils/Events";
 import Config from "../Config";
 import { prefix, data } from "../utils/Utils";
 
-register("chat", (player) => {
+onChatPacket((player) => {
     if (!Config.crystalTimer || !Dungeon.inDungeon || player !== Player.getName()) return
-    let timeTaken = Date.now() - Dungeon.bossEntry
+    
+    const timeTaken = Date.now() - Dungeon.bossEntry
     let msg = `${prefix} &aCrystal took &b${Math.round(timeTaken / 10) / 100}s`
     if (!data.crystalPB || timeTaken < data.crystalPB) {
         msg += " &d&l(PB)"
@@ -12,4 +14,4 @@ register("chat", (player) => {
         data.save()
     }
     new Message(new TextComponent(msg).setHover("show_text", `&aPersonal Best: &b${Math.round(data.crystalPB / 10)/100}s`)).chat()
-}).setCriteria("${player} picked up an Energy Crystal!")
+}).setCriteria(/^(\w+) picked up an Energy Crystal!$/)
